8aecdb22b2284cdbfb49a21b6fb7d7401f6da235
Ryan Golhar <golharam@Ryans-iMac.local>
2025-03-19 13:21:20 -0400

    Fix import order and pylint issue

--- application.py
+++ application.py
@@ -1,20 +1,22 @@
 ''' NGS360 API Server '''
+from sqlalchemy.sql import text
+
 from apiserver import create_app
 
 # this is needed to register the models for flask-migrate / Alembic migrations
 from apiserver import models # pylint: disable=unused-import
 
 from apiserver import db
-from sqlalchemy.sql import text
 
 app = create_app()
 
 @app.route('/healthcheck')
 def healthcheck():
+    ''' Healthcheck endpoint '''
     try:
         db.session.query(text('1')).from_statement(text('SELECT 1')).all()
         return '<h1>It works.</h1>'
-    except Exception as e:
+    except Exception as e: # pylint: disable=broad-exception-caught
         # e holds description of the error
         error_text = "<p>The error:<br>" + str(e) + "</p>"
         hed = '<h1>Something is broken.</h1>'
