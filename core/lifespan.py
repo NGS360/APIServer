b4ddee26becbbddebbb9ba2bdd2932d3a3344f33
Ryan Golhar <ryan.golhar@bms.com>
2025-07-25 15:03:41 -0400

    Remove db table creation/dropping from lifespan

--- core/lifespan.py
+++ core/lifespan.py
@@ -2,26 +2,29 @@
 Define application startup and shutdown procedures
 """
 from fastapi import FastAPI
-from core.config import get_settings
-from core.db import init_db, drop_tables
+#from core.config import get_settings
+#from core.db import init_db, drop_tables
 from core.logger import logger
 
 # Handle startup/shutdown tasks
 async def lifespan(app: FastAPI):
   # Startup
+  logger.info("In lifespan...starting up")
   # Initialize database (if not done already)
-  try:
-    logger.info("Initializing database, %s", get_settings().SQLALCHEMY_DATABASE_URI_MASKED_PASSWORD)
-    init_db()
-    logger.info("Database initialized successfully")
-  except Exception as e:
-    logger.error(f"Database initialization failed: {e}")
+  #try:
+  #  logger.info("Initializing database, %s", get_settings().SQLALCHEMY_DATABASE_URI_MASKED_PASSWORD)
+  #  init_db()
+  #  logger.info("Database initialized successfully")
+  #except Exception as e:
+  #  logger.error(f"Database initialization failed: {e}")
     # Re-raise the exception to fail application startup
-    raise RuntimeError(f"Cannot start application: database initialization failed - {str(e)}")
+  #  raise RuntimeError(f"Cannot start application: database initialization failed - {str(e)}")
 
+  logger.info("In lifespan...yield")
   yield
-  # Shutdown
 
+  # Shutdown
+  logger.info("In lifespan...shutting down")
   # Destroy database
-  logger.info("Dropping database tables, %s", get_settings().SQLALCHEMY_DATABASE_URI_MASKED_PASSWORD)
-  drop_tables()
+  #logger.info("Dropping database tables, %s", get_settings().SQLALCHEMY_DATABASE_URI_MASKED_PASSWORD)
+  #drop_tables()
