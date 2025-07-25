cab2a1b42cc0f36fef8a8988b67f630adadd370c
Ryan Golhar <ryan.golhar@bms.com>
2025-07-25 15:19:02 -0400

    Resolve warning about async generator function lifespans are deprecated, use an @contextlib.asynccontextmanager function instead

--- core/lifespan.py
+++ core/lifespan.py
@@ -1,12 +1,14 @@
 """
 Define application startup and shutdown procedures
 """
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 #from core.config import get_settings
 #from core.db import init_db, drop_tables
 from core.logger import logger
 
 # Handle startup/shutdown tasks
+@asynccontextmanager
 async def lifespan(app: FastAPI):
   # Startup
   logger.info("In lifespan...starting up")
@@ -21,10 +23,11 @@ async def lifespan(app: FastAPI):
   #  raise RuntimeError(f"Cannot start application: database initialization failed - {str(e)}")
 
   logger.info("In lifespan...yield")
-  yield
-
-  # Shutdown
-  logger.info("In lifespan...shutting down")
-  # Destroy database
-  #logger.info("Dropping database tables, %s", get_settings().SQLALCHEMY_DATABASE_URI_MASKED_PASSWORD)
-  #drop_tables()
+  try:
+    yield
+  finally:
+    # Shutdown
+    logger.info("In lifespan...shutting down")
+    # Destroy database
+    #logger.info("Dropping database tables, %s", get_settings().SQLALCHEMY_DATABASE_URI_MASKED_PASSWORD)
+    #drop_tables()
