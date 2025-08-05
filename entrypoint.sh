ef32f675f5282f458ea070521238ba8d185ab98e
Ryan Golhar <golharam@Ryans-iMac.local>
2025-08-05 15:58:52 -0400

    Fix docker-compose and entrypoint to update database on boot

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-echo "Upgrading database..."
-alembic upgrade head
+echo "Upgrading database, $SQLALCHEMY_DATABASE_URI..."
+uv run alembic upgrade head
 
 echo "Starting FastAPI server..."
 exec uv run uvicorn main:app --host 0.0.0.0 --port 5000 --reload
