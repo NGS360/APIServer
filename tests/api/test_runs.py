177d00ed21d023f394f52a88421ae3b85fcd144b
Ryan Golhar <golharam@Ryans-iMac.local>
2025-08-16 10:06:48 -0400

    Test that we can fetch a run

--- tests/api/test_runs.py
+++ tests/api/test_runs.py
@@ -1,6 +1,7 @@
 '''
 Test /runs endpoint
 '''
+import datetime
 from fastapi.testclient import TestClient
 from sqlmodel import Session
 
@@ -18,3 +19,34 @@ def test_get_runs(client: TestClient, session: Session):
         'has_next': False,
         'has_prev': False
     }
+
+    # Add a run to the database
+    from api.runs.models import SequencingRun
+    from uuid import uuid4
+    new_run = SequencingRun(
+        id=uuid4(),
+        run_date=datetime.date(2019, 1, 10),
+        machine_id='MACHINE123',
+        run_number='RUN001',
+        flowcell_id='FLOWCELL123',
+        experiment_name='Test Experiment',
+        s3_run_folder_path='s3://bucket/path/to/run',
+        status='completed',
+        run_time='24',
+    )
+    session.add(new_run)
+    session.commit()
+
+    # Test get runs again
+    response = client.get('/api/v1/runs')
+    assert response.status_code == 200
+    data = response.json()
+    assert data['total_items'] == 1
+    assert data['data'][0]['machine_id'] == 'MACHINE123'
+    assert data['data'][0]['run_number'] == 'RUN001'
+    assert data['data'][0]['flowcell_id'] == 'FLOWCELL123'
+    assert data['data'][0]['experiment_name'] == 'Test Experiment'
+    assert data['data'][0]['s3_run_folder_path'] == 's3://bucket/path/to/run'
+    assert data['data'][0]['status'] == 'completed'
+    assert data['data'][0]['run_time'] == '24'
+    assert data['data'][0]['barcode'] == '20190110_24_MACHINE123_FLOWCELL123_RUN001'
