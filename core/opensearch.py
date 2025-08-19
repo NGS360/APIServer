963fcc99f5b3190a873139911842cb591ccd39ce
Ryan Golhar <golharam@Ryans-iMac.local>
2025-08-19 10:28:48 -0400

    Ad get_opensearch_client method

--- core/opensearch.py
+++ core/opensearch.py
@@ -4,31 +4,40 @@ OpenSearch configuration
 from opensearchpy import OpenSearch
 from core.config import get_settings
 from core.logger import logger
-INDEXES = ["projects", "samples"]
+INDEXES = ["projects", "samples", "illumina_runs"]
 
-# Connect to opensearch
-if get_settings().OPENSEARCH_USER and get_settings().OPENSEARCH_PASSWORD:
-    auth = (get_settings().OPENSEARCH_USER, get_settings().OPENSEARCH_PASSWORD)
-else:
-    auth = None
+client = None
 
-if get_settings().OPENSEARCH_HOST is None:
-    client = None
-else:
-    client = OpenSearch(
-        hosts = [{
-            'host': get_settings().OPENSEARCH_HOST,
-            'port': get_settings().OPENSEARCH_PORT}],
-        http_compress = True, # enables gzip compression for request bodies
-        http_auth = auth,
-        #use_ssl = True,
-        #verify_certs = True,
-        #ssl_assert_hostname = False,
-        #ssl_show_warn = False,
-        #ca_certs = ca_certs_path
-    )
+def get_opensearch_client():
+    global client
 
-def init_indexes():
+    if client:
+        return client
+
+    # Connect to opensearch
+    if get_settings().OPENSEARCH_USER and get_settings().OPENSEARCH_PASSWORD:
+        auth = (get_settings().OPENSEARCH_USER, get_settings().OPENSEARCH_PASSWORD)
+    else:
+        auth = None
+
+    if get_settings().OPENSEARCH_HOST is None:
+        client = None
+    else:
+        client = OpenSearch(
+            hosts = [{
+                'host': get_settings().OPENSEARCH_HOST,
+                'port': get_settings().OPENSEARCH_PORT}],
+            http_compress = True, # enables gzip compression for request bodies
+            http_auth = auth,
+            #use_ssl = True,
+            #verify_certs = True,
+            #ssl_assert_hostname = False,
+            #ssl_show_warn = False,
+            #ca_certs = ca_certs_path
+        )
+    return client
+
+def init_indexes(client):
     if client is None:
         return
 
