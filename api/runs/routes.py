b93ab295cd6458ab09c3ce7e98f1d4ed5bc7555b
Ryan Golhar <golharam@Ryans-iMac.local>
2025-09-03 16:07:57 -0400

    Make projects and runs endpoint match in ordering

--- api/runs/routes.py
+++ api/runs/routes.py
@@ -25,30 +25,6 @@ import api.runs.services as services
 
 router = APIRouter(prefix="/runs", tags=["Run Endpoints"])
 
-@router.get(
-    "",
-    response_model=SequencingRunsPublic,
-    status_code=status.HTTP_200_OK,
-    tags=["Run Endpoints"]
-)
-def get_runs(
-  session: SessionDep, 
-  page: int = Query(1, description="Page number (1-indexed)"), 
-  per_page: int = Query(20, description="Number of items per page"),
-  sort_by: str = Query('project_id', description="Field to sort by"),
-  sort_order: Literal['asc', 'desc'] = Query('asc', description="Sort order (asc or desc)")
-) -> SequencingRunsPublic:
-    """
-    Retrieve a list of all sequencing runs.
-    """
-    return services.get_runs(
-        session=session, 
-        page=page,
-        per_page=per_page,
-        sort_by=sort_by,
-        sort_order=sort_order
-    )
-
 @router.post(
   "",
   response_model=SequencingRunPublic,
@@ -79,6 +55,30 @@ def add_run(
     run_time=run.run_time,
     barcode=run.barcode
   )
+
+@router.get(
+    "",
+    response_model=SequencingRunsPublic,
+    status_code=status.HTTP_200_OK,
+    tags=["Run Endpoints"]
+)
+def get_runs(
+  session: SessionDep, 
+  page: int = Query(1, description="Page number (1-indexed)"), 
+  per_page: int = Query(20, description="Number of items per page"),
+  sort_by: str = Query('project_id', description="Field to sort by"),
+  sort_order: Literal['asc', 'desc'] = Query('asc', description="Sort order (asc or desc)")
+) -> SequencingRunsPublic:
+    """
+    Retrieve a list of all sequencing runs.
+    """
+    return services.get_runs(
+        session=session, 
+        page=page,
+        per_page=per_page,
+        sort_by=sort_by,
+        sort_order=sort_order
+    )
 
 @router.get(
     "/search",
