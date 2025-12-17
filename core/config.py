20ff4aa68f29879b3ce8394cf67cbea81da70bd8
Ryan Golhar <golharam@Ryans-iMac.local>
2025-12-17 13:40:37 -0500

    Remove trailing slash in env vars

--- core/config.py
+++ core/config.py
@@ -157,9 +157,9 @@ class Settings(BaseSettings):
     AWS_REGION: str | None = os.getenv("AWS_REGION")
 
     # Bucket configurations
-    DATA_BUCKET_URI: str = os.getenv("DATA_BUCKET_URI", "s3://my-data-bucket/")
-    RESULTS_BUCKET_URI: str = os.getenv("RESULTS_BUCKET_URI", "s3://my-results-bucket/")
-    CONFIGS_BUCKET_URI: str = os.getenv("TOOL_CONFIGS_BUCKET_URI", "s3://my-tool-configs-bucket/")
+    DATA_BUCKET_URI: str = os.getenv("DATA_BUCKET_URI", "s3://my-data-bucket")
+    RESULTS_BUCKET_URI: str = os.getenv("RESULTS_BUCKET_URI", "s3://my-results-bucket")
+    CONFIGS_BUCKET_URI: str = os.getenv("TOOL_CONFIGS_BUCKET_URI", "s3://my-tool-configs-bucket")
 
     # Read environment variables from .env file, if it exists
     # extra='ignore' prevents validation errors from extra env vars
