916ec501cc5ede571dfb2a34316ad768cf934f33
vasques1 <Steven.VasquezGrinnell@bms.com>
2026-01-29 16:47:59 -0500

    add file models

---
+++ api/filerecord/__init__.py
@@ -0,0 +1,7 @@
+"""
+FileRecord module - reusable file metadata records.
+
+This module provides a polymorphic file reference system that can associate
+file metadata (URI, size, hashes, tags) with various entity types like
+QCRecord, Sample, etc.
+"""
