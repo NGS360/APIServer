d0f6a9d9ebdf605dfe25eab701259780d81b4aaf
Ryan Golhar <ryan.golhar@bms.com>
2026-02-13 14:49:36 -0500

    Removing unneeded __init__.py

--- api/qcmetrics/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-QCMetrics module - Quality control metrics from pipeline executions.
-
-This module provides models and APIs for storing and retrieving QC metrics
-from bioinformatics pipeline runs.
-"""
