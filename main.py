9e0d43cb168b806245234620f063d045c26dc20c
Ryan Golhar <ryan.golhar@bms.com>
2026-07-06 11:48:00 -0400

    Add blank line for lint

--- main.py
+++ main.py
@@ -35,6 +35,7 @@ from api.users.routes import router as users_router
 
 logger = logging.getLogger(__name__)
 
+
 # Customize route id's
 # Helpful for creating sensible names in the client
 def custom_generate_unique_id(route: APIRoute):
